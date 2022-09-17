4b37f854fbd2f40b84fe17eeebc9fc21aa316ca9
mono-glyceride <glyceride.mono@gmail.com>
2022-09-17 15:08:03 +0000

    add SerchConditonとItemのリレーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,4 @@
 class Item < ApplicationRecord
+	has_many :owned_informations, class_name: 'SerchCondition', foreign_key: 'owned_item_id'
+  has_many :wanted_informations, class_name: 'SerchCondition', foreign_key: 'wanted_item_id'
 end
--- app/models/serch_condition.rb
+++ app/models/serch_condition.rb
@@ -1,3 +1,5 @@
 class SerchCondition < ApplicationRecord
 	belogns_to :user
+	belongs_to :wanted_item, class_name: 'Item', optional: true
+  belongs_to :owned_item,  class_name: 'Item', optional: true
 end
