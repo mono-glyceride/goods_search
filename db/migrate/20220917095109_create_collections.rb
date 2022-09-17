63e4d2fd2a40014f29dfe1413d5ad897cb81ed45
mono-glyceride <glyceride.mono@gmail.com>
2022-09-17 14:12:16 +0000

    fix character_tweet_mapsからcollectionsにテーブル名変更

--- db/migrate/20220917095109_create_collections.rb
+++ db/migrate/20220917095109_create_collections.rb
@@ -1,11 +1,11 @@
-class CreateCharacterTweetMaps < ActiveRecord::Migration[6.0]
+class CreateCollections < ActiveRecord::Migration[6.0]
   def change
-    create_table :character_tweet_maps do |t|
+    create_table :collection do |t|
       t.references :tweet, foreign_key: true, null: false
       t.references :character_user_want, foreign_key: { to_table: 'characters' }
       t.references :character_user_have, foreign_key: { to_table: 'characters' }
       t.timestamps
     end
-    add_index  :character_tweet_maps, [:tweet, :character_user_want, :character_user_have], unique: true
+    add_index  :collection, [:tweet, :character_user_want, :character_user_have], unique: true
   end
 end
