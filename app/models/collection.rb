459db25f3445942315c2fb2a2a06cb31a88e2ea8
mono-glyceride <glyceride.mono@gmail.com>
2022-09-17 15:47:29 +0000

    add ItemとCollectionのリレーション

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,2 +1,6 @@
 class Collection < ApplicationRecord
+	#ツイッタラーに需要あるグッズ
+	belongs_to :demanding, class_name: 'Item', optional: true
+	#ツイッタラーが供給するグッズ
+  belongs_to :supplying, class_name: 'Item', optional: true
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
+	# ユーザが持っているグッズ
 	has_many :owned_informations, class_name: 'SerchCondition', foreign_key: 'owned_item_id'
+  # ユーザが欲しいグッズ
   has_many :wanted_informations, class_name: 'SerchCondition', foreign_key: 'wanted_item_id'
+  # ツイッタラーに需要あるグッズ
+  has_many :demands, class_name: 'Collection', foreign_key: 'demanding'
+  # ツイッタラーが供給するグッズ
+  has_many :supplies, class_name: 'Collection', foreign_key: 'supplying'
+  # owned_informations == demands && wanted_information == supplies の時に取引が成立する公算
 end
