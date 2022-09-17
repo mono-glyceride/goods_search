6425b34a0a3bf408013f5851c66a9a02b8b3ad25
mono-glyceride <glyceride.mono@gmail.com>
2022-09-17 14:26:05 +0000

    update 中間テーブルの名前を複数形に変形

--- db/migrate/20220917095109_create_collections.rb
+++ db/migrate/20220917095109_create_collections.rb
@@ -1,11 +1,11 @@
 class CreateCollections < ActiveRecord::Migration[6.0]
   def change
-    create_table :collection do |t|
+    create_table :collections do |t|
       t.references :tweet, foreign_key: true, null: false
       t.references :wanted_item, foreign_key: { to_table: 'items' }
       t.references :owned_item, foreign_key: { to_table: 'items' }
       t.timestamps
     end
-    add_index  :collection, [:tweet, :wanted_item, :owned_item], unique: true
+    add_index  :collections, [:tweet, :wanted_item, :owned_item], unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,23 +12,23 @@
 
 ActiveRecord::Schema.define(version: 2022_09_17_095109) do
 
-  create_table "characters", force: :cascade do |t|
-    t.string "name", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["name"], name: "index_characters_on_name"
-  end
-
-  create_table "collection", force: :cascade do |t|
+  create_table "collections", force: :cascade do |t|
     t.integer "tweet_id", null: false
     t.integer "wanted_item_id"
     t.integer "owned_item_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index "\"tweet\", \"wanted_item\", \"owned_item\"", name: "index_collection_on_tweet_and_wanted_item_and_owned_item", unique: true
-    t.index ["owned_item_id"], name: "index_collection_on_owned_item_id"
-    t.index ["tweet_id"], name: "index_collection_on_tweet_id"
-    t.index ["wanted_item_id"], name: "index_collection_on_wanted_item_id"
+    t.index "\"tweet\", \"wanted_item\", \"owned_item\"", name: "index_collections_on_tweet_and_wanted_item_and_owned_item", unique: true
+    t.index ["owned_item_id"], name: "index_collections_on_owned_item_id"
+    t.index ["tweet_id"], name: "index_collections_on_tweet_id"
+    t.index ["wanted_item_id"], name: "index_collections_on_wanted_item_id"
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_items_on_name"
   end
 
   create_table "keywords", force: :cascade do |t|
@@ -39,13 +39,13 @@ ActiveRecord::Schema.define(version: 2022_09_17_095109) do
 
   create_table "serch_conditions", force: :cascade do |t|
     t.integer "user_id", null: false
-    t.integer "character_user_want_id"
-    t.integer "character_user_have_id"
+    t.integer "wanted_item_id"
+    t.integer "owned_item_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["character_user_have_id"], name: "index_serch_conditions_on_character_user_have_id"
-    t.index ["character_user_want_id"], name: "index_serch_conditions_on_character_user_want_id"
+    t.index ["owned_item_id"], name: "index_serch_conditions_on_owned_item_id"
     t.index ["user_id"], name: "index_serch_conditions_on_user_id"
+    t.index ["wanted_item_id"], name: "index_serch_conditions_on_wanted_item_id"
   end
 
   create_table "tweets", force: :cascade do |t|
@@ -79,10 +79,10 @@ ActiveRecord::Schema.define(version: 2022_09_17_095109) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "collection", "items", column: "owned_item_id"
-  add_foreign_key "collection", "items", column: "wanted_item_id"
-  add_foreign_key "collection", "tweets"
-  add_foreign_key "serch_conditions", "characters", column: "character_user_have_id"
-  add_foreign_key "serch_conditions", "characters", column: "character_user_want_id"
+  add_foreign_key "collections", "items", column: "owned_item_id"
+  add_foreign_key "collections", "items", column: "wanted_item_id"
+  add_foreign_key "collections", "tweets"
+  add_foreign_key "serch_conditions", "items", column: "owned_item_id"
+  add_foreign_key "serch_conditions", "items", column: "wanted_item_id"
   add_foreign_key "serch_conditions", "users"
 end
