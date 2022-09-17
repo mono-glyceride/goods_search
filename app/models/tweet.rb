ab5fb8bc5147bb2643948880a96dabd8e9f2b230
mono-glyceride <glyceride.mono@gmail.com>
2022-09-17 16:42:54 +0000

    add Tweetモデルのバリデーション

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,17 @@
 class Tweet < ApplicationRecord
 	has_many :collections, dependent: :destroy
+	
+	with_options length: { maximum: 255 } do
+		with_options presence: true do
+	    validates :param, uniqueness: true
+	    validates :tweet_user_name
+	    validates :tweet_user_icon
+	    validates :body
+    end
+    validates :image_1
+    validates :image_2
+    validates :image_3
+    validates :image_4
+  end
+  validates :send, inclusion: { in: [true, false] }
 end
