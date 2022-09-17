835a73e1e06fb309d29b68e76714f56bacff87a1
mono-glyceride <glyceride.mono@gmail.com>
2022-09-17 15:50:15 +0000

    add CollectionとTweetのリレーション

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -3,4 +3,5 @@ class Collection < ApplicationRecord
 	belongs_to :demanding, class_name: 'Item', optional: true
 	#ツイッタラーが供給するグッズ
   belongs_to :supplying, class_name: 'Item', optional: true
+  belogns_to :tweet
 end
