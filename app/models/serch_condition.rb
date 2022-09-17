06b502410eb362d5522b790a30752c02f02d7b14
mono-glyceride <glyceride.mono@gmail.com>
2022-09-17 16:37:10 +0000

    add SerchConditionモデルのバリデーション

--- app/models/serch_condition.rb
+++ app/models/serch_condition.rb
@@ -2,4 +2,12 @@ class SerchCondition < ApplicationRecord
 	belogns_to :user
 	belongs_to :wanted_item, class_name: 'Item', optional: true
   belongs_to :owned_item,  class_name: 'Item', optional: true
+
+  validates :keywords, length: { maximum: 255 }
+  validates :user_id, presence: true
+  validate :required_either_wanted_item_or_owned_item
+  def required_either_email_or_phone
+    return if wanted_item_id.present? || owned_item_id.present?
+    errors.add(:item_information, '求めるグッズと譲るグッズのどちらか一方、あるいは両方をご入力ください')
+  end
 end
