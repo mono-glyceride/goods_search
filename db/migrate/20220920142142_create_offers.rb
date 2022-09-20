ef50f4550a0e5d85e1dfa49eb49fe819cf1b7271
mono-glyceride <glyceride.mono@gmail.com>
2022-09-20 14:30:55 +0000

    fix offersの外部キーの記述を主修正

--- db/migrate/20220920142142_create_offers.rb
+++ db/migrate/20220920142142_create_offers.rb
@@ -1,8 +1,8 @@
 class CreateOffers < ActiveRecord::Migration[6.0]
   def change
     create_table :offers do |t|
-      t.references :combination_id, foreign_key: true, null: false
-      t.references :tweet_id, foreign_key: true, null: false
+      t.references :combinations, foreign_key: true, null: false
+      t.references :tweets, foreign_key: true, null: false
       t.timestamps
     end
     add_index :offers, [:combination_id, :tweet_id], unique: true
