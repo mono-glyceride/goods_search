132a3e684dbd759ce5bf72ac770d8538e531066b
mono-glyceride <glyceride.mono@gmail.com>
2022-09-20 14:33:18 +0000

    remove collection

--- app/models/collection.rb
+++
@@ -1,9 +0,0 @@
-class Collection < ApplicationRecord
-	#ツイッタラーに需要あるグッズ
-	belongs_to :demanding, class_name: 'Item', optional: true
-	#ツイッタラーが供給するグッズ
-  belongs_to :supplying, class_name: 'Item', optional: true
-  belongs_to :tweet
-  
-  validates :tweet_id, presence: true
-end
