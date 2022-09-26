a4b7993918e1c4d09849f5ba476b55b0a07dc6b3
mono-glyceride <glyceride.mono@gmail.com>
2022-09-26 15:36:47 +0000

    update itemモデルにバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,11 @@ class Item < ApplicationRecord
   has_many :items_registrations, dependent: :restrict_with_error
   has_many :registrations, through: :items_registrations
   has_many :offers, dependent: :destroy
-  validates :name, presence: true, uniqueness: true, length: { maximum: 255 }
+  
+  with_options presence: true do
+    with_options length: { maximum: 255 } do
+      validates :wanted_item, uniqueness: { scope: :wanted_item }
+      validates :owned_item
+    end
+  end
 end
