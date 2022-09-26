919b33411c9f713da4f71abf85ebe5c8b6d69b7d
mono-glyceride <glyceride.mono@gmail.com>
2022-09-26 15:03:31 +0000

    update configモデルにバリデーション

--- app/models/config.rb
+++ app/models/config.rb
@@ -1,3 +1,6 @@
 class Config < ApplicationRecord
   belongs_to :user
+  
+  validates :notify_by_dm, inclusion: {in: [true, false]}
+  validates :user_id, presence: true, uniqueness: true, numericality: {only_integer: true, greater_than_or_equal_to: 0}
 end
