f48a90ad4eb033710e9bcb0be7f16711c93b851c
mono-glyceride <glyceride.mono@gmail.com>
2022-09-26 15:34:08 +0000

    update item-registrationモデルにバリデーション

--- app/models/items_registration.rb
+++ app/models/items_registration.rb
@@ -1,4 +1,7 @@
 class ItemsRegistration < ApplicationRecord
   belongs_to :registration
   belongs_to :items
+  
+  validates :registrations_id, presence: true
+  validates :items_id, presence: true, uniqueness: { scope: :registrations_id }
 end
