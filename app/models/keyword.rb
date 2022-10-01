25c21381f5657c6a9383e81fc0f8515a0497557f
mono-glyceride <glyceride.mono@gmail.com>
2022-10-01 16:06:03 +0000

    rubocop 自動修正

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -1,6 +1,6 @@
 class Keyword < ApplicationRecord
   belongs_to :registration
-  
+
   validates :registration_id, presence: true
   validates :keyword, presence: true, length: { maximum: 255 }
 end
