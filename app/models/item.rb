60ded07e75ee1ffac328b86bc76575704122ff67
mono-glyceride <glyceride.mono@gmail.com>
2022-11-24 15:23:42 +0000

    add フォームの入力値を保存するメソッドを追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,8 +5,15 @@ class Item < ApplicationRecord
 
   with_options presence: true do
     with_options length: { maximum: 255 } do
-      validates :wanted_item, uniqueness: { scope: :wanted_item }
+      validates :wanted_item, uniqueness: { scope: :owned_item }
       validates :owned_item
+    end
+  end
+
+  def self.save_items(wanted_item, owned_item_list)
+    owned_item_list.each do |owned_item|
+      item = Item.new(wanted_item: wanted_item, owned_item: owned_item)
+      item.save if item.valid?
     end
   end
 end
--- app/models/registration.rb
+++ app/models/registration.rb
@@ -6,4 +6,20 @@ class Registration < ApplicationRecord
   has_many :matches, dependent: :destroy
 
   validates :user_id, presence: true
+
+  def save_keyword(keyword_list)
+    keyword_list.each do |keyword|
+      @keyword = self.keywords.build
+      @keyword.keyword = keyword
+      @keyword.save if @keyword.valid?
+    end
+  end
+
+  def save_items_registrations(item_ids)
+    item_ids.each do |item_id|
+      items_registrations = self.items_registrations.build
+      items_registrations.item_id = item_id
+      items_registrations.save if items_registrations.valid?
+    end
+  end
 end
