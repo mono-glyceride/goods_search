8faaaa93215c14de1ccf726e8c6bf4325b8fed91
mono-glyceride <glyceride.mono@gmail.com>
2022-11-24 15:25:24 +0000

    add formオブジェクトのコントローラ

---
+++ app/controllers/merchandise_forms_controller.rb
@@ -0,0 +1,44 @@
+class MerchandiseFormsController < ApplicationController
+  before_action :set_merchandise_form, only: %i[edit update destroy]
+  before_action :authenticate_user!
+
+  def index
+  end
+
+  def new
+    @merchandise_form = MerchandiseForm.new
+  end
+
+  def create
+    @merchandise_form = MerchandiseForm.new(merchandise_form_params)
+    if @merchandise_form.valid?
+      if @merchandise_form.save(current_user)
+      end
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @merchandise_form = MerchandiseForm.new(merchandise_form: @merchandise_form)
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+    def set_merchandise_form
+      @merchandise_form = merchandise_form.find(params[:id])
+    end
+
+    def merchandise_form_params
+      params.require(:merchandise_form).permit(:wanted_merchandise, :owned_merchandises, :keywords)
+    end
+
+    def set_keyword_list
+    	keywords.split(' ')
+  	end
+end
